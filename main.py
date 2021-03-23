ff63c3f0799979ca23b80f0341fa670579f94791
KungPaoChick <matthewriel63@gmail.com>
2021-03-23 20:00:03 +0000

    Added new features

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import csv
+import argparse
+import colorama
 
 
 def get_api(link):
@@ -10,14 +12,10 @@ def get_api(link):
     with open('public-api.json', 'w') as f_source:
         json.dump(source, f_source, indent=2)
 
-    return no_auth_apis()
-
 
 def no_auth_apis():
     with open('public-api.json') as j_source:
         source = json.load(j_source)
-
-    print(f"Total API's: {source['count']}")
     
     with open('no-auth.csv', 'w') as f:
         headers = ['Link', 'API', 'HTTPS', 'Cors', 'Category']
@@ -33,6 +31,40 @@ def no_auth_apis():
                 write.writerows([no_auth_data])
 
 
+def apis():
+    with open('public-api.json') as j_source:
+        source = json.load(j_source)
+
+    print(f"Total APIs: {source['count']}")
+    with open('All-APIs.csv', 'w') as f:
+        headers = ['#', 'Link', 'API', 'Auth', 'HTTPS', 'Cors', 'Category']
+        writer = csv.writer(f, dialect='excel')
+        writer.writerow(headers)
+
+        for index, apis in enumerate(source['entries'], start=1):
+            data = [index, apis['Link'], apis['API'], apis['Auth'],
+                    apis['HTTPS'], apis['Cors'], apis['Category']]
+            writer.writerows([data])
+
+
 if __name__ == '__main__':
-    get_api('https://api.publicapis.org/entries')
+    colorama.init()
+    get_api('https://api.publicapis.org/entries')    
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                    description="Lists APIs")
+
+    parser.add_argument('-na', '--noauth',
+                        metavar='NOAUTH',
+                        help="Gets APIs that require no Authentication.")
     
+    parser.add_argument('-a', '--all',
+                        action='store_true',
+                        help="Lists down all APIs.")
+
+    args = parser.parse_args()
+
+    if args.noauth:
+        no_auth_apis()
+
+    if args.all:
+        apis()
